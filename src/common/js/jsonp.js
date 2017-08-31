3cb077069e43d7b00df13a324171fc5fc6b20c93
tlx6550 <912730337@qq.com>
2017-08-31 16:08:32 +0800

    jsonp 封装 常用参数配置config

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -14,7 +14,17 @@ export default function jsonp(url, data, option) {
   })
 }
 
-export function param(data) {
+/* export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    encodeURIComponent() 函数[1] 可把字符串作为URI 组件进行编码。
+    encodeURIComponent("Hello world!")=>Hello%20world!
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+} */
+function param(data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
