89cf8b70e8898f4310cac2d31f7fcafe8b28465a
tlx6550 <912730337@qq.com>
2017-08-31 17:36:33 +0800

    理解把请求参数和url拼接在一起，模拟真实请求数据

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,5 @@
 import jsonp from 'common/js/jsonp'
-import {commonParams,options} from './config'
+import {commonParams, options} from './config'
 export function getRecommend () {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   const data = Object.assign({}, commonParams, {
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -6,6 +6,7 @@ export default function jsonp(url, data, option) {
   return new Promise((resolve, reject) => {
     originJsonp(url, option, (err, data) => {
       if (!err) {
+        // 把请求服务器返回的数据data，塞进resolve中，以便then回掉使用
         resolve(data)
       } else {
         reject(err)
@@ -25,10 +26,13 @@ export default function jsonp(url, data, option) {
   return url ? url.substring(1) : ''
 } */
 function param(data) {
+  /* data={"g_tk":1928093487,"inCharset":"utf-8","outCharset":"utf-8","notice":0,"format":"jsonp","platform":"h5","uin":0,"needNewCode":1} */
   let url = ''
   for (var k in data) {
+    console.log('k=' + k)
     let value = data[k] !== undefined ? data[k] : ''
     url += '&' + k + '=' + encodeURIComponent(value)
   }
+  /* url=&g_tk=1928093487&inCharset=utf-8&outCharset=utf-8&notice=0&format=jsonp&platform=h5&uin=0&needNewCode=1 */
   return url ? url.substring(1) : ''
 }
